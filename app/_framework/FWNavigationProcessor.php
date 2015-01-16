7fadf66158326b217c9e44f1d6ddf3c08c57df67
rGunti <raphael.guntersweiler@hotmail.ch>
2015-01-16 19:06:56 +0100

    Added "Page Source Last Updated"

--- app/_framework/FWNavigationProcessor.php
+++ app/_framework/FWNavigationProcessor.php
@@ -10,10 +10,20 @@
 
 Logger::debug("Requested Path: " . NavigationPath::getCurrentPath());
 
+$pageSource = "";
+$showLastUpdateTime = TRUE;
+
 switch (NavigationPath::getCurrentPath()) {
-    case '/' : include_once Utils::getServerBasePath() . '/page/' . AppConfig::APP_NAV_DEFAULT_PAGE_SCRIPT; break;
-    case '/Home': include_once Utils::getServerBasePath() . '/page/_home.php'; break;
-    case '/Log': include_once Utils::getServerBasePath() . '/page/_log.php'; break;
-    case '/Info': include_once Utils::getServerBasePath() . '/page/_info.php'; break;
-    default: include_once Utils::getServerBasePath() . '/page/_404.php'; break;
+    case '/' : $pageSource = Utils::getServerBasePath() . '/page/' . AppConfig::APP_NAV_DEFAULT_PAGE_SCRIPT; break;
+    case '/Home': $pageSource = Utils::getServerBasePath() . '/page/_home.php'; break;
+    case '/Log': $pageSource = Utils::getServerBasePath() . '/page/_log.php'; break;
+    case '/Info': $pageSource = Utils::getServerBasePath() . '/page/_info.php'; break;
+    default: $pageSource = Utils::getServerBasePath() . '/page/_404.php'; break;
 }
+include_once $pageSource;
+
+if ($showLastUpdateTime) { ?>
+<p class="lastUpdated">
+    Page Source Last Updated: <?= date("d.m.Y H:i:s", filemtime($pageSource)) ?>
+</p>
+<?php }
--- app/_framework/Version.php
+++ app/_framework/Version.php
@@ -14,5 +14,5 @@
  * @author Raphael
  */
 class FrameworkVersion {
-    const VERSION = "Chiyo 0.1";
+    const VERSION = "Chiyo 0.1.1";
 }
